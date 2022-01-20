da49bc894b8e5c271ec555756639893554c9d1bb
lihugang <94808542+lihugang@users.noreply.github.com>
2022-01-20 14:23:36 +0800

    Put file (GitHubVPN API)

--- js/page.js
+++ js/page.js
@@ -6,11 +6,46 @@ function loadBasicPage() {
 function loadBasicHTML() {
     window.isLoadingBasicHTML = true;
     var obj = parseURLHash();
-    document.body.innerHTML = "<center><h1>GitNovel</h1></center>"
+    var username = getusername();
+    document.body.innerHTML = fillTemplate(basicHTML.basic + basicHTML[obj.page], [
+        [
+            "username",
+            username
+        ]
+    ]);
+};
+
+function loadPage() {
+    if (window.isLoadingBasicHTML) {
+        loadBasicHTML();
+    } else loadBasicPage();
+};
+
+function parseURLHash() {
+    var hash = location.href.substring(location.origin.length);
+    if (hash.substring(0, 9) == "/404.html")
+        hash = location.hash;
+    return {
+        page: hash.split("@")[0] || "index",
+        arg: hash.split("@")[1] || ""
+    };
+};
+
+function fillTemplate(string, options) {
+    for (var i = 0; i < options.length; i++) {
+        while (string.indexOf("{{$" + options[i][0] + "}}") !== -1) {
+            string = string.replace("{{$" + options[i][0] + "}}", options[i][1]);
+        };
+    };
+    return string;
 };
 
 basicHTML = {
+    basic: `
+    <center><h1>GitNovel</h1></center><hr>用户名<span id=username>{{$username}}</span> <a href=# onclick=javascript:logout();>退出登录</a><br>
+    `,
     index: `
-    
+    <hr>图书馆<a href=/library>戳我</a><br>
+    我的书籍<a href=/mybook>戳我</a><br>
     `
 };
