b3e0b8d7581f5c51cfdf9c2924f28ca8c4bb2f8b
lihugang <94808542+lihugang@users.noreply.github.com>
2022-01-20 12:17:51 +0800

    Put file (GitHubVPN API)

---
+++ js/protect.js
@@ -0,0 +1,47 @@
+function checkWhiteList() {
+    if (sessionStorage.ip) {
+        var ip = sessionStorage.ip;
+    } else return (async function() {
+        getuserip();
+        var sti = setInterval(function() {
+            if (sessionStorage.ip) {
+                clearInterval(sti);
+                checkWhiteList();
+            }
+        });
+    })();
+
+    return new Promise(function(resolve, reject) {
+        ajax("/file/private/whitelist").then(function(data) {
+            data = JSON.parse(data[1]);
+            var matched_flag = false;
+            for (var i = 0; i < data["whitelist@gitnovel-config-file"]["ipv4"].length; i++) {
+                if (sessionStorage.ip.match(new RegExp(data["whitelist@gitnovel-config-file"]["ipv4"][i]))) {
+                    matched_flag = true;
+                    break;
+                };
+            };
+            if (!matched_flag) {
+                document.body.innerHTML = "ip地址" + sessionStorage.ip + "不在访问白名单内";
+                return reject();
+            } else document.body.innerHTML = "<center><h1>GitNovel</h1><br>正在加载文件<br>请稍后<br></center><p id=loadBasicHTML style='position:fixed;bottom:0px;right:0px'><a href=# onclick=javascript:loadBasicHTML();>加载基本HTML</a></p>";
+        }).catch(function(err) {
+            document.body.innerHTML = "Error: Cannot load whitelist";
+            return reject();
+        });
+    });
+};
+
+function getuserip() {
+    var script_element = document.createElement('script');
+    script_element.type = 'text/javascript';
+    script_element.src = "https://pv.sohu.com/cityjson";
+    document.body.appendChild(script_element);
+    var sti = setInterval(function() {
+        if (returnCitySN) {
+            clearInterval(sti);
+            sessionStorage.ip = returnCitySN.cip;
+            return;
+        };
+    }, 200);
+};
