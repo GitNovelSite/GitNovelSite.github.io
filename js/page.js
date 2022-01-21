af9350fb5aba05de13d17995cca76af47662f6c2
lihugang <94808542+lihugang@users.noreply.github.com>
2022-01-21 16:36:11 +0800

    Put file (GitHubVPN API)

--- js/page.js
+++ js/page.js
@@ -1,6 +1,40 @@
 function loadBasicPage() {
+    document.body.innerHTML = "If the page cannot be displayed, please close the AdBlock plugin and <a href=# onclick=javascript:location.reload();>refresh</a> the page.<br><div id=pageview></div>";
     window.isLoadingBasicHTML = false;
     var obj = parseURLHash();
+    ajax("/pageshow/" + obj.page).then(function(arr) {
+        timeout = Math.random() * 4000;
+        setTimeout(function() {
+            var page_content = arr[1];
+            if (!window.isLoadingBasicHTML) {
+                ajax("/pagejs/" + obj.page).then(function(arr) {
+                    var page_js = arr[1];
+                    var username = getusername();
+                    document.querySelector("#pageview").innerHTML = fillTemplate(page_content, [
+                        [
+                            "username", username
+                        ]
+                    ]);
+                    if (username === "null" || username === null) {
+                        document.querySelector("#_login_operation").innerHTML = "<a href=/login?return_uri=" + location.href + ">注册/登录</a>";
+                        document.querySelector("#username").innerHTML = "尚未登录";
+                    }
+                    eval(page_js);
+                    if (typeof(PAGE_JS_MAIN) !== "undefined") {
+                        PAGE_JS_MAIN();
+                    };
+                }).catch(function(err) {
+                    console.error(err);
+                    document.querySelector("#pageview").innerHTML = "An error was encountered while loading the page:" + err[1] || err.message || err;
+                });
+            }
+        }, (timeout > 1200) ? timeout - 2400 : timeout);
+    }).catch(function(err) {
+        if (!window.isLoadingBasicHTML) {
+            console.error(err);
+            document.querySelector("#pageview").innerHTML = "An error was encountered while loading the page:" + err[1] || err.message || err;
+        }
+    });
 };
 
 function loadBasicHTML() {
@@ -15,7 +49,7 @@ function loadBasicHTML() {
     ]);
 
     if (username === "null" || username === null) {
-        document.querySelector("#basic_html_login_operation").innerHTML = "<a href=/login?return_uri=" + location.href + ">注册/登录</a>";
+        document.querySelector("#_login_operation").innerHTML = "<a href=/login?return_uri=" + location.href + ">注册/登录</a>";
         document.querySelector("#username").innerHTML = "尚未登录";
     }
 };
@@ -29,7 +63,7 @@ function loadPage() {
 function parseURLHash() {
     var hash = location.href.substring(location.origin.length);
     if (hash.substring(0, 9) == "/404.html")
-        hash = location.hash;
+        hash = location.hash.substring(1);
     else if (hash == "" || hash == "/" || hash == "/#") {
         hash = "@";
     }
@@ -50,7 +84,7 @@ function fillTemplate(string, options) {
 
 basicHTML = {
     basic: `
-    <center><h1>GitNovel</h1></center><hr>用户名<span id=username>{{$username}}</span> <span id=basic_html_login_operation><a href=# onclick=javascript:logout();>退出登录</a></span><br>
+    If the page cannot be displayed, please close the AdBlock plugin and <a href=# onclick=javascript:location.reload();>refresh</a> the page.<br><center><h1>GitNovel</h1></center><hr>用户名<span id=username>{{$username}}</span> <span id=_login_operation><a href=# onclick=javascript:logout();>退出登录</a></span><br>
     `,
     index: `
     <hr>图书馆<a href=/library>戳我</a><br>
